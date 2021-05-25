a36316ffaa4aa67c03033cd4ac3270b6494a568c
Craig Neff <ifly12610@gmail.com>
2021-05-24 20:23:06 -0600

    added an about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <p>
+        {" "}
+        this is a mock e-commerce website I made for a final project in my
+        coding boot camp.{" "}
+      </p>
+    </div>
+  );
+};
